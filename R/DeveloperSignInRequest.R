49130a65e63c4e45b81bc09f26cf9d58b1791d59
Sebastian <ses@damvad.com>
2017-09-17 12:55:30 +0000

    First commit

---
+++ R/DeveloperSignInRequest.R
@@ -0,0 +1,46 @@
+#' Obtain Access token
+#'
+#' With this endpoint you can obtain Access Token required to access secured endpoints.
+#' To do so, you have to provide API Key & API Secret.
+#' They can be generated on the profile website (https://profile.timeular.com/#/login).
+#'
+#' @param apiKey Your API key
+#' @param apiSecret Your secret API key
+#'
+#' @return An Access Token required to access secured endpoints
+#' @export
+#'
+#' @examples apiKey <- "ABCDefgh1234="
+#' apiSecret <- "EFGHijkl5678="
+#' DeveloperSignInRequest(apiKey, apiSecret)
+DeveloperSignInRequest <- function(apiKey, apiSecret) {
+
+    base_url <- "api.timeular.com/api/v1/"
+
+    # Building the query string
+    queryString <- list(
+        apiKey = apiKey,
+        apiSecret = apiSecret
+    )
+
+    # Post query string to url
+    resp <- httr::POST(
+        url = paste0(base_url, "developer/sign-in"),
+        body = queryString,
+        encode = "json"
+    )
+
+    if ( httr::status_code(resp) == 200 ) {
+
+        # Extract content from the post data
+        parsed <- httr::content(resp, type = "application/json")
+        token <- parsed$token
+
+    } else {
+
+        stop("Something went wrong!")
+
+    }
+
+    return(token)
+}
